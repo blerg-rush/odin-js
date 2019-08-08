b3586a1a345df4129697aa8757922de55c50761c
Will Gabriel <will.gabriel@gmail.com>
2019-08-08 07:24:31 -0600

    Create repeatString()

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string,number) {
+    let str = '';
+    if (number < 0) str = 'ERROR';
+    else{
+        for(i = 0; i < number; i++){
+            str += string;
+        }
+    }
+    return str;
 }
 
 module.exports = repeatString
