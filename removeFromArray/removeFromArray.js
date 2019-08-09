7fe822a7c999cbe64b1e4b4f053f2a596393a093
Will Gabriel <will.gabriel@gmail.com>
2019-08-08 18:57:36 -0600

    Create loops remove from anArray on match found

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,8 +1,14 @@
 const removeFromArray = function(input, ...values) {
     let anArray = Array.from(input);
     for(i = 0; i < values.length; i++){
-        
+        for(j = 0; j < anArray.length; j++){
+            if(values[i] == anArray[j]){
+                anArray.splice(j,1);
+                i--;
+            }
+        }
     }
+    return anArray;
 }
 
 module.exports = removeFromArray
