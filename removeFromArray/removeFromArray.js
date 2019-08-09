97a354c3fc78d3e4c9b3c4da5509a77af862c6d1
Will Gabriel <will.gabriel@gmail.com>
2019-08-08 18:46:28 -0600

    Convert removeFromArray input to array

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,8 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(input, ...values) {
+    let anArray = Array.from(input);
+    for(i = 0; i < values.length; i++){
+        
+    }
 }
 
 module.exports = removeFromArray
