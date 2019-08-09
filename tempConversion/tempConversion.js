fdfd1a555f4cc6d309c41559717c463308ada50a
Will Gabriel <will.gabriel@gmail.com>
2019-08-08 20:46:44 -0600

    Change rounding method to return number

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,13 +1,13 @@
 const ftoc = function(fahren) {
   let celsi = ((fahren - 32) * (5 / 9));
   if (celsi % 1 == 0) return celsi;
-  return celsi.toFixed(1);
+  return Math.round(celsi * 10) / 10;
 }
 
 const ctof = function(celsi) {
   let fahren = ((celsi * (9 / 5) + 32));
   if (fahren % 1 == 0) return fahren;
-  return fahren.toFixed(1);
+  return Math.round(fahren * 10) / 10;
 }
 
 module.exports = {
