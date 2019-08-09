9a4f59014400cc9c38e285fdcbd26d2567bdf4aa
Will Gabriel <will.gabriel@gmail.com>
2019-08-08 19:50:18 -0600

    Return 'ERROR' if sumAll parameters not numbers

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,6 +1,6 @@
 const sumAll = function(num1, num2) {
     let total = 0;
-    if (num1 < 0 || num2 < 0) return 'ERROR';
+    if (num1 < 0 || num2 < 0 || typeof num1 != 'number' || typeof num2 != 'number') return 'ERROR';
     if (num1 > num2) [num1, num2] = [num2, num1];
     for (i = num1; i <= num2; i++){
         total += i;
