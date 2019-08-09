38c4cf88acd1662dbd94facb488e05b8ec777082
Will Gabriel <will.gabriel@gmail.com>
2019-08-08 19:40:12 -0600

    Create sumAll function

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,9 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let total = 0;
+    for (i = num1; i <= num2; i++){
+        total += i;
+    }
+    return total;
 }
 
 module.exports = sumAll
