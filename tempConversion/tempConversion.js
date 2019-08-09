b7338627f18dc1297fa957a7d562fe2f947327ef
Will Gabriel <will.gabriel@gmail.com>
2019-08-08 20:41:30 -0600

    Return temperature to one decimal place if not int

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
 const ftoc = function(fahren) {
-  return Math.round((fahren - 32) * (5 / 9));
+  let celsi = ((fahren - 32) * (5 / 9));
+  if (celsi % 1 == 0) return celsi;
+  return celsi.toFixed(1);
 }
 
 const ctof = function(celsi) {
-  return Math.round((celsi * (9 / 5) + 32));
+  let fahren = ((celsi * (9 / 5) + 32));
+  if (fahren % 1 == 0) return fahren;
+  return fahren.toFixed(1);
 }
 
 module.exports = {
