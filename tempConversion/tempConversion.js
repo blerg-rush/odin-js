057e07db04d9413d03178e68339bab36209b938a
Will Gabriel <will.gabriel@gmail.com>
2019-08-08 20:29:20 -0600

    Create ctof function

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -2,8 +2,8 @@ const ftoc = function(fharen) {
   return round((farhen - 32) * (5 / 9));
 }
 
-const ctof = function() {
-
+const ctof = function(celsi) {
+  return round(celsi * (9 / 5) + 32);
 }
 
 module.exports = {
