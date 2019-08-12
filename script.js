bea5f746edb22da100801d9240c99a4951f6553b
Will Gabriel <will.gabriel@gmail.com>
2019-08-11 21:26:26 -0600

    Add score updating, checking, and game reset

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const results = document.querySelector('#results');
 const comp = document.querySelector('#comp');
+const scoreboard = document.querySelector('#scoreboard');
 const buttons = document.querySelectorAll('button');
+let pScore = 0;
+let cScore = 0;
 
 buttons.forEach((button) => {
     button.addEventListener('click', (e) => {
@@ -20,43 +23,83 @@ function computerPlay(){
 }
 
 function playRound(pSelect, cSelect){
-    comp.textContent = `Computer played ${cSelect}`;
+    comp.textContent = `Computer played ${cSelect}.`;
     if(pSelect == 'Rock'){
         switch(cSelect){
-            case 'Rock': results.textContent = draw(pSelect);
-            break;
-            case 'Paper': results.textContent = lose(pSelect, cSelect);
-            break;
-            case 'Scissors': results.textContent = win(pSelect, cSelect);
+            case 'Rock':
+                results.textContent = draw(pSelect);
+                break;
+            case 'Paper':
+                results.textContent = lose(pSelect, cSelect);
+                break;
+            case 'Scissors':
+                results.textContent = win(pSelect, cSelect);
         }
     }else if(pSelect == 'Paper'){
         switch(cSelect){
-            case 'Rock': results.textContent = win(pSelect, cSelect);
-            break;
-            case 'Paper': results.textContent = draw(pSelect);
-            break;
-            case 'Scissors': results.textContent = lose(pSelect, cSelect);
+            case 'Rock':
+                results.textContent = win(pSelect, cSelect);
+                break;
+            case 'Paper':
+                results.textContent = draw(pSelect);
+                break;
+            case 'Scissors':
+                results.textContent = lose(pSelect, cSelect);
         }
     }else{
         switch(cSelect){
-            case 'Rock': results.textContent = lose(pSelect, cSelect);
-            break;
-            case 'Paper': results.textContent = win(pSelect, cSelect);
-            break;
+            case 'Rock':
+                results.textContent = lose(pSelect, cSelect);
+                break;
+            case 'Paper':
+                results.textContent = win(pSelect, cSelect);
+                break;
             case 'Scissors': results.textContent = draw(pSelect);
         }
     }
 }
 
+function updateScore(result){
+    switch(result){
+        case 'win':
+            pScore++;
+            break;
+        case 'lose':
+            cScore++;
+            break;        
+    }
+    checkScore();
+}
+
+function checkScore(){
+    if (pScore == 5){
+        scoreboard.textContent = `You beat the computer ${pScore} - ${cScore}! Try again?`
+        reset();
+    }else if (cScore == 5){
+        scoreboard.textContent = `The computer beat you ${cScore} - ${pScore}! Try again?`
+        reset();
+    }else{
+        scoreboard.textContent = `The score is ${pScore} - ${cScore}.`;
+    }
+}
+
+function reset(){
+    pScore = 0;
+    cScore = 0;
+}
+
 function win(pSelect,cSelect){
+    updateScore('win');
     return `You win! ${pSelect} beats ${cSelect}`;
 }
 
 function lose(pSelect,cSelect){
+    updateScore('lose');
     return `You lose. ${cSelect} beats ${pSelect}`;
 }
 
 function draw(pSelect){
+    updateScore('draw');
     return `You draw. Both played ${pSelect}`
 }
 
