7139ca9f3ddc19308db675b44cf96c45ffe399d3
Will Gabriel <will.gabriel@gmail.com>
2019-08-12 20:36:23 -0600

    Add default CSS grid

--- script.js
+++ script.js
@@ -1,19 +1,41 @@
 const content = document.querySelector('#content');
 let gridSize = 16;
-let gridWidth = 400;
+let gridWidth = 500;
+let squareSize = 30;
+
+content.style.width = `${gridWidth}px`;
+content.style.height = `${gridWidth}px`;
+content.style.display = 'grid';
+content.style.gridGap = '0px';
+content.style.justifyContent = 'center';
+content.style.alignContent = 'center';
+content.style.gridTemplateColumns = `repeat(${gridSize}, auto)`
+content.style.gridTemplateRows = `repeat(${gridSize}, auto)`
 
 function createSquare(x,y){
     const square = document.createElement('div');
     square.setAttribute('id', `${x},${y}`);
     square.classList.add('square');
+    style(square);
     content.appendChild(square);
 }
 
-function createGrid(size){
-
+function createGrid(gridSize){
+    for(i=0 ; i<gridSize; i++){
+        for(j=0 ; j<gridSize; j++){
+            createSquare(j,i);
+        }
+    }
 }
 
-function calculateSize(){
+function style(square){
+    square.style.border = '1px solid black';
+    square.style.width = `${squareSize - 2}px`;
+    square.style.height = `${squareSize - 2}px`;
+    square.style.display = 'inline-block';
+}
+
+function setSquareSize(){
 
 }
 
