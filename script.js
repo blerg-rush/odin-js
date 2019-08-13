d2b5726b6fdc3a8b8073803db9cfbf5c14c289ce
Will Gabriel <will.gabriel@gmail.com>
2019-08-12 19:01:26 -0600

    Initialize needed variables and functions

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 const content = document.querySelector('#content');
+let gridSize = 16;
+let gridWidth = 400;
 
 function createSquare(x,y){
     const square = document.createElement('div');
     square.setAttribute('id', `${x},${y}`);
     square.classList.add('square');
     content.appendChild(square);
+}
+
+function createGrid(size){
+
+}
+
+function calculateSize(){
+
+}
+
+function draw(){
+
 }
