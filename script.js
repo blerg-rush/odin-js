572f1cced9af7ab408ded4761f2697fd7fadd40f
Will Gabriel <will.gabriel@gmail.com>
2019-08-12 21:07:15 -0600

    Remove overlapping square borders

--- script.js
+++ script.js
@@ -3,21 +3,16 @@ let gridSize = 16;
 let gridWidth = 500;
 let squareSize = Math.floor(gridWidth / gridSize);
 
-content.style.width = `${gridWidth}px`;
-content.style.height = `${gridWidth}px`;
-content.style.display = 'grid';
-content.style.gridGap = '0px';
-content.style.justifyContent = 'center';
-content.style.alignContent = 'center';
-content.style.gridTemplateColumns = `repeat(${gridSize}, auto)`
-content.style.gridTemplateRows = `repeat(${gridSize}, auto)`
-
-function createSquare(x,y){
-    const square = document.createElement('div');
-    square.setAttribute('id', `${x},${y}`);
-    square.classList.add('square');
-    style(square);
-    content.appendChild(square);
+function createContainer(gridSize){
+    content.style.width = `${gridWidth}px`;
+    content.style.height = `${gridWidth}px`;
+    content.style.display = 'grid';
+    content.style.gridGap = '0px';
+    content.style.justifyContent = 'center';
+    content.style.alignContent = 'center';
+    content.style.gridTemplateColumns = `repeat(${gridSize}, auto)`
+    content.style.gridTemplateRows = `repeat(${gridSize}, auto)`
+    createGrid(gridSize);
 }
 
 function createGrid(gridSize){
@@ -28,11 +23,21 @@ function createGrid(gridSize){
     }
 }
 
-function style(square){
-    square.style.border = '1px solid black';
+function createSquare(x,y){
+    const square = document.createElement('div');
+    square.setAttribute('id', `${x},${y}`);
+    square.classList.add('square');
+    style(square,x,y);
+    content.appendChild(square);
+}
+
+function style(square,x,y){
     square.style.width = `${squareSize - 2}px`;
     square.style.height = `${squareSize - 2}px`;
     square.style.display = 'inline-block';
+    square.style.border = '1px solid black';
+    if(x > 0) square.style.borderLeft = 'none';
+    if(y > 0) square.style.borderTop = 'none';
 }
 
 function setSquareSize(){
