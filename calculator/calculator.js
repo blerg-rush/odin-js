d115f672eb93bb72d6fe3ddecca75517c6ad0d8b
TheWillGabriel <will.gabriel@gmail.com>
2019-08-31 11:41:14 -0600

    Complete calculator exercise

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,25 +1,41 @@
-function add () {
-	
+function add (x,y) {
+	return x + y;
 }
 
-function subtract () {
-	
+function subtract (x,y) {
+	return x - y;
 }
 
-function sum () {
-	
+function sum (array) {
+	let result = 0
+	array.forEach(function(number) {
+		result += number;
+	});
+	return result;
 }
 
-function multiply () {
-	
+function multiply (array) {
+	let result = array[0];
+	array.shift();
+	array.forEach(function(number) {
+		result *= number;
+	});
+	return result;
 }
 
-function power() {
-	
+function power(x,y) {
+	return x ** y;
 }
 
-function factorial() {
-	
+function factorial(x) {
+	let result = 1;
+	if(x==0) return 1;
+	else{
+		for(i=x; i>1; i--){
+			result *= i;
+		}
+	}
+	return result
 }
 
 module.exports = {
