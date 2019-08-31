67a7fb9948e5f1f6bff55f9ef28d76997270dd96
TheWillGabriel <will.gabriel@gmail.com>
2019-08-31 12:55:02 -0600

    Complete fibonacci sequencer

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(x) {
+  x = parseInt(x);
+  let fib = [1, 1];
+  let result = 0;
+  if(x < 1) return "OOPS"
+  if (x == 1) return 1;
+  if (x == 2) return 1;
+  for(i=2; i<x; i++){
+    result = fib[i-1] + fib[i-2];
+    fib.push(result);
+  }
+  return result;
 }
 
 module.exports = fibonacci
