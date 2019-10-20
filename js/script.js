c532b1462da07fe587b4ff9467651cd712e347f7
TheWillGabriel <will.gabriel@gmail.com>
2019-10-20 14:06:37 -0600

    Add create book card function

--- js/script.js
+++ js/script.js
@@ -18,6 +18,37 @@ function addBookToLibrary (book) {
   myLibrary.push(book)
 }
 
+function createBookCard (book) {
+  const card = document.createElement('div')
+  card.className = 'card'
+
+  const cardBody = document.createElement('div')
+  cardBody.className = 'card-body'
+
+  const title = document.createElement('h5')
+  title.className = 'card-title'
+  title.innerText = book.title
+
+  const author = document.createElement('h6')
+  author.className = 'card-subtitle mb-2 text-muted'
+  author.innerText = book.author
+
+  const pages = document.createElement('p')
+  pages.className = 'card-text'
+  pages.innerText = `pages: ${book.pages}`
+
+  const read = document.createElement('p')
+  read.clasName = 'card-text'
+  read.innerText = `read: ${read ? 'yes' : 'no'}`
+
+  cardBody.appendChild(title)
+  cardBody.appendChild(author)
+  cardBody.appendChild(pages)
+  cardBody.appendChild(read)
+  card.appendChild(cardBody)
+  return card
+}
+
 function render (library) {
   const booksList = document.querySelector('.books')
   library.forEach(book => {
